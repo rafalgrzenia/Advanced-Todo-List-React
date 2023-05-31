a0f0ba65b27c989719437fee0861c51a375e8871
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-05-31 13:39:52 +0200

    Reset All the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,15 @@
-import { useState } from "react";
+import { AddTodo } from "./AddTodo";
+import { ToDoItem } from "./TodoItem";
 import "./styles.css";
-import { TodoItem } from "./TodoItem";
-import { FormFilter } from "./FormFilter";
-import { useLocalStorage } from "./useLocalStorage";
 
 function App() {
-  const [newTodoName, setNewTodoName] = useState("");
-  const [todos, setTodos] = useLocalStorage("TODO_NAME", []);
-
-  function addNewTodo() {
-    if (newTodoName === "") return;
-
-    setTodos((currentTodos) => {
-      return [
-        ...currentTodos,
-        { name: newTodoName, completed: false, id: crypto.randomUUID() },
-      ];
-    });
-    setNewTodoName("");
-  }
-
-  function toggleTodo(todoId, completed) {
-    setTodos((currentTodos) => {
-      return currentTodos.map((todo) => {
-        if (todo.id === todoId) return { ...todo, completed };
-
-        return todo;
-      });
-    });
-  }
-
-  function deleteTodo(todoId) {
-    setTodos((currentTodos) => {
-      return currentTodos.filter((todo) => todo.id !== todoId);
-    });
-  }
-
-  return (
-    <>
-      <FormFilter />
-      <ul id="list">
-        {todos.map((todo) => {
-          return (
-            <TodoItem
-              key={todo.id}
-              {...todo}
-              toggleTodo={toggleTodo}
-              deleteTodo={deleteTodo}
-            />
-          );
-        })}
-      </ul>
-
-      <div id="new-todo-form">
-        <label htmlFor="todo-input">New Todo</label>
-        <input
-          type="text"
-          id="todo-input"
-          value={newTodoName}
-          onChange={(e) => setNewTodoName(e.target.value)}
-        />
-        <button onClick={addNewTodo}>Add Todo</button>
-      </div>
-    </>
-  );
+ return (
+  <>
+  <ul id="list">
+    <ToDoItem />
+  </ul>
+  <AddTodo />
+  </>
+ )
 }
-
 export default App;

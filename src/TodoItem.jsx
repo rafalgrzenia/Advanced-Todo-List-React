9d3ef9bc5aec032d9a85d72401af5e3494e04f47
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-06-06 13:55:39 +0200

    Update TodoItem component

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,9 +1,9 @@
-export function TodoItem() {
+export function TodoItem({name}) {
   return (
     <li className="list-item">
       <label className="list-item-label">
         <input type="checkbox" data-list-item-checkbox />
-        <span data-list-item-text>Item 1</span>
+        <span data-list-item-text>{name}</span>
       </label>
       <button data-button-edit>Edit</button>
       <button data-button-delete>Delete</button>

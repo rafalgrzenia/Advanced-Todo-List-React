beffab4819dd3d27b8e68844e8c0888616ce0d5d
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-06-06 13:56:02 +0200

    Add states and functions to App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+import { TodoForm } from "./TodoForm";
 import { TodoItem } from "./TodoItem";
 import "./styles.css";
 
 function App() {
+  const [newTodoName, setnewTodoName] = useState("");
+  const [todoList, setNewTodoList] = useState([]);
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
+
+  function addTodoItem(e) {
+    e.preventDefault();
+    setNewTodoList((currentTodos) => {
+      return [...currentTodos, { name: newTodoName }];
+    });
+  }
+
   return (
-    <TodoItem />
-  )
+    <>
+      <ul id="list">
+        {todoList.map(todo => <TodoItem name={todo.name} />)}
+      </ul>
+      <TodoForm setnewTodoName={setnewTodoName} addTodoItem={addTodoItem} />
+    </>
+  );
 }
 export default App;

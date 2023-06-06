521bb565e31d93f3cec8e3516db91ac91c332da4
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-06-06 13:39:55 +0200

    Remove existing project and add new intial files

--- src/AddTodo.jsx
+++
@@ -1,9 +0,0 @@
-export function AddTodo() {
-  return (
-    <form id="new-todo-form">
-      <label for="todo-input">New Todo</label>
-      <input type="text" id="todo-input" />
-      <button>Add Todo</button>
-    </form>
-  );
-}
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-import { AddTodo } from "./AddTodo";
-import { ToDoItem } from "./TodoItem";
 import "./styles.css";
 
 function App() {
- return (
-  <>
-  <ul id="list">
-    <ToDoItem />
-  </ul>
-  <AddTodo />
-  </>
- )
+  
 }
 export default App;

974db8588ef4df0631f9148642c6cd9181ecf215
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-06-06 15:12:50 +0200

    Create FilterTodoList component

--- src/FilterTodoList.jsx
+++ src/FilterTodoList.jsx
@@ -1,9 +1,10 @@
-export function FilterTodoList() {
-  return (
+export function FilterTodoList({setFilteredTodoName}) {
+  
+    return (
     <div className="filter-form">
       <div className="filter-form-group">
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" value="" />
+        <input type="text" id="name" onChange={e => setFilteredTodoName(e.target.value)}/>
       </div>
       <label>
         <input type="checkbox" />

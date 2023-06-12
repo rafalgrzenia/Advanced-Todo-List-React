ba44220704445609a8e53b284cd48c51a8978465
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-06-12 13:25:17 +0200

    Add toggletodo onchange in TodoItem component

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,11 +1,18 @@
-export function TodoItem({id, name, deleteTodoItem}) {
+export function TodoItem({ id, completed, name, deleteTodoItem, toggleTodo }) {
   return (
     <li className="list-item">
       <label className="list-item-label">
-        <input type="checkbox" data-list-item-checkbox />
+        <input
+          type="checkbox"
+          data-list-item-checkbox
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
         <span data-list-item-text>{name}</span>
       </label>
-      <button data-button-delete onClick={() => deleteTodoItem(id)}>Delete</button>
+      <button data-button-delete onClick={() => deleteTodoItem(id)}>
+        Delete
+      </button>
     </li>
   );
 }
